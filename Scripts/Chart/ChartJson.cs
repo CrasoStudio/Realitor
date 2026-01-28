c120eda91b68e1ee84eff108c0400b3236702370
xiaob <apple19782013@163.com>
2026-01-28 14:41:55 +0800

    Initial commit

--- Scripts/Chart/ChartJson.cs
+++ Scripts/Chart/ChartJson.cs
@@ -1,28 +1,34 @@
 using System.Collections.Generic;
 
-namespace Old;
+/*
+ 注：谱面中出现的位置坐标范围为-1到1，XZ轴移动长度在任何地方都相同，Y轴移动范围中，
+    天空note：-1为地面轨道平面，0为天空轨道默认高度，1为摄像机对应高度；
+    摄像机：-1为地面轨道平面，0为摄像机默认高度；
+    轨道略
+*/
 
 public class ChartJson
 {
-    // ReSharper disable FieldCanBeMadeReadOnly.Global
     public float BPM;
+    public float offset;
+    public int version = 1;
     
-    public List<Note> notes = new();
-    public List<SkyTrack> skyTracks = new();
-        
-    public List<SpeedGroup> speedGroups = new();
-    public List<BPMEvent> BPMEvents = new();
+    public List<GroundTrack> groundTracks = [];
+    public List<SkyTrack> skyTracks = [];
     
-    public TrackEvents trackEvents;
-    public CameraEvents cameraEvents;
+    public List<BPMEvent> BPMEvents = [];
+    public List<SpeedGroup> speedGroups = [];
+    
+    public List<CameraMoveEvent> cameraMoveEvents = [];
+    public List<CameraRotateEvent> cameraRotateEvents = [];
+    public List<CameraBrightnessEvent> cameraBrightnessEvents = [];
 }
 
 public class Note
 {
     public float time;
-    public int type;//tap=0,hold=1,ltap=2
-    public int track;//0-3
     public float duration;
+    public int type;// tap=0,hold=1
     public float speed = 1;
     public int speedGroupID;
 }
@@ -30,14 +36,23 @@ public class Note
 public class SkyNote
 {
     public float time;
-    public int type;//tap=0,flick=1
+    public int type;// tap=0,flick=1,catch=2
+}
+
+public class GroundTrack
+{
+    public int track;// 在 0-3 范围外为装饰轨道
+    public List<Note> notes;
+    public List<TrackMoveEvent> trackMoveEvents = [];
+    public List<TrackRotateEvent> trackRotateEvents = [];
+    public List<TrackTransparencyEvent> trackTransparencyEvents = [];
 }
 
 public class SkyTrack
 {
     public int track;
-    public List<TrackPoint> points = new();
-    public List<SkyNote> notes = new();
+    public List<TrackPoint> points = [];
+    public List<SkyNote> notes = [];
 }
 
 public class TrackPoint
@@ -45,6 +60,12 @@ public class TrackPoint
     public float time;
     public float x, y;
     public int speed;
+}
+
+public class SpeedGroup
+{
+    public int id;
+    public List<SpeedEvent> events = [];
 }
 
 public class SpeedEvent
@@ -58,19 +79,6 @@ public class BPMEvent
 {
     public float startTime;
     public float BPM;
-}
-
-public class SpeedGroup
-{
-    public int id;
-    public List<SpeedEvent> events = new();
-}
-
-public class CameraEvents
-{
-    public List<CameraMoveEvent> cameraMoveEvents = new();
-    public List<CameraRotateEvent> cameraRotateEvents = new();
-    public List<CameraBrightnessEvent> cameraBrightnessEvents = new();
 }
 
 public class CameraMoveEvent
@@ -108,16 +116,8 @@ public class CameraBrightnessEvent
     public EasingMode easing;
 }
 
-public class TrackEvents
-{
-    public List<TrackMoveEvent> trackMoveEvents = new();
-    public List<TrackRotateEvent> trackRotateEvents = new();
-    public List<TrackTransparencyEvent> trackTransparencyEvents = new();
-}
-
 public class TrackMoveEvent
 {
-    public int track;
     public float startTime;
     public float endTime;
     public float startXPos;
@@ -131,7 +131,6 @@ public class TrackMoveEvent
 
 public class TrackRotateEvent
 {
-    public int track;
     public float startTime;
     public float endTime;
     public float startXRotate;
@@ -145,7 +144,6 @@ public class TrackRotateEvent
 
 public class TrackTransparencyEvent
 {
-    public int track;
     public float startTime;
     public float endTime;
     public float startValue;
